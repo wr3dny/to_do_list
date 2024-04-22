cbf5e76937a60a1ddda52df43d98f5eae566a851
wr3dny <pawel.pawtel@gmail.com>
2024-04-22 13:20:36 +0200

    In progress 22/04

--- to_doscript.py
+++ to_doscript.py
@@ -1,5 +1,39 @@
+import json
+
+
 def to_do(text):
     print(f"TO DO: {text}")
+
+
+def read_file():
+    with open('tasks.txt', 'r') as file:
+        tasks = file.readlines()
+    return tasks
+
+
+def task_name():
+    task = input("Enter the task name: ")
+    return task
+
+
+def write_file(task):
+    with open('tasks.txt', 'a') as file:
+        json.dump(task, file)
+        file.write(task + '\n')
+
+def menu():
+    print("1. Add a task")
+    print("2. Remove a task")
+    print("3. List all tasks")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+    while choice not in ['1', '2', '3', '4']:
+        print("Invalid choice")
+        choice = input("Enter your choice: ")
+    return choice
+
+
 
 
 def options():
@@ -13,6 +47,7 @@ def options():
 
 
 def main():
+    print(read_file())
     options()
     match = {
         '1': 'add a task',
